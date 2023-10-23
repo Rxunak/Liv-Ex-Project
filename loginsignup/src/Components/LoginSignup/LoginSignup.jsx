bdbb0ad6be3a1d41aaf8c5943211d580b84238c6
Rxunak <raunakuk2016@gmail.com>
2023-10-23 14:43:28 +0100

    Completed Html for the login and sign up page

--- loginsignup/src/Components/LoginSignup/LoginSignup.jsx
+++ loginsignup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import "./LoginSignup.css";
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import user_icon from "../Assets/person.png";
+import email_icon from "../Assets/email.png";
+import password_icon from "../Assets/password.png";
 
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="Inputs">
+        <div className="Input">
+          <img src={user_icon} alt="" />
+          <input type="text" />
+        </div>
+        <div className="Input">
+          <img src={email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="Input">
+          <img src={password_icon} alt="" />
+          <input type="password" />
+        </div>
+        <div className="forgot-password">
+          Forgot Password? <span>Click Here!!</span>
+        </div>
+        <div className="SubmitContainer">
+          <div className="submit">Sign Up</div>
+          <div className="submit">Login</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
