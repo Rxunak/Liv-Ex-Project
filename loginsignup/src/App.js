23bcc4340c32e2c7d2caf59f4c7210151143af92
Rxunak <raunakuk2016@gmail.com>
2023-10-24 16:33:06 +0100

    Wokring on creating and connecting multiple pages

--- loginsignup/src/App.js
+++ loginsignup/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
+import InformationPage from './Components/InformationPage/Information'
 
 function App() {
   return (
     <div >
      <LoginSignup/>
+     <InformationPage/>
     </div>
   );
 }
