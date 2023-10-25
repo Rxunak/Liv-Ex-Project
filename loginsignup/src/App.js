1723bbde8bab20db3d2c88073f4e8a27ded81109
Rxunak <raunakuk2016@gmail.com>
2023-10-25 06:45:20 +0100

    Update App.js

--- loginsignup/src/App.js
+++ loginsignup/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import LoginSignup from '../src/Component/LoginSignup';
-import InformationPage from '../src/Component/Information'
+import InformationPage from '../src/Component/Information';
 
 function App() {
   return (
