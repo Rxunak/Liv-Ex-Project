cf7bbc58a6f7ead5cb03b77efa88ef79a8f472e5
Rxunak <raunakuk2016@gmail.com>
2023-10-26 16:41:52 +0100

    Working on Navbar

--- loginsignup/src/App.js
+++ loginsignup/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import LoginSignup from '../src/Component/LoginSignup';
-import InformationPage from '../src/Component/Information';
+
 
 function App() {
   return (
     <div >
      <LoginSignup/>
-     <InformationPage/>
+ 
     </div>
   );
 }
