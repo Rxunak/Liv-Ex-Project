c05baee4aaa220a0e276a4c3b437ed8abb393e33
Rxunak <raunakuk2016@gmail.com>
2023-11-09 12:39:16 +0000

    working on Home page

--- loginsignup/src/Routes/Home.js
+++ loginsignup/src/Routes/Home.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import '../Styles/Home.css'
 import Navbar from '../Component/Navbar'
 
-
+import logo from '../Assets/ITHome.jpg'
 
 const Home = () => {
   return (
     <div>
       <div>    <Navbar/></div>
-      <div>
-        <h1>This is my Home page</h1>
-      
-      
 
+      <div >
+        <h1 id='homehead'>Welcome to <br />
+        Help Desk Suppport</h1>
+      </div>
+
+      <div className='infoo'>
+        <p id='info'>Hey we are here <br />
+         for your support hjsbhsdbhsdcbsd <br />
+         cshd csdc sdc sdc sdc sdjc sdc sjcsdjc</p>
+      </div>
+
+      <div >
+      <img className="HomeLogo" src={logo} alt="" />
       </div>
 
     </div>
