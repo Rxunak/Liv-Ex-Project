b2ad1b90dff4d4c525b768db7a0f80bef8857a3d
Rxunak <raunakuk2016@gmail.com>
2023-11-10 16:26:09 +0000

    wokring on home page

--- loginsignup/src/Routes/Home.js
+++ loginsignup/src/Routes/Home.js
@@ -7,37 +7,26 @@ import '../Styles/Home.css'
 
 
 import logo from '../Assets/ITHome.jpg'
-function Home (){
-  return(
+function Home() {
+  return (
     <div className='home-container'>
 
-       <div className='heading'>
-        <h1>Welcome to IT Support</h1>
+      <div className='heading'>
+        <h1 className='head1'>Welcome to IT Support</h1>
       </div>
 
       <div className='home-banner'>
-        <img className='home-image' src={logo} alt="" />
+
       </div>
 
       
 
-     
 
- 
-
-  
-
-
-
-      
-
-      
-    
     </div>
-    
 
-   
-  
+
+
+
   )
 }
 
