a3c3aa277dca5a7d2c2d7d16d5cc5400c7330ffb
Rxunak <raunakuk2016@gmail.com>
2023-11-16 17:49:51 +0000

    Working on footer and home page

--- loginsignup/src/Component/Footer.js
+++ loginsignup/src/Component/Footer.js
@@ -1,76 +1,46 @@
-  import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../Styles/Footer.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Styles/Footer.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-
-import logo from '../Assets/IT Support.png'
+import logo from "../Assets/IT Support.png";
 
 const Footer = () => {
-    return (
-        <div className='main-footer'>
-            <div className='footercontainer'>
-
-                <div className='row1'>
-
-                    <div className='Socials'>
-                        <h3 className='socialh3'>Socials Follow</h3>
-                        <a id='ic' href="https://facebook.com" target='_blank'>
-                            <FontAwesomeIcon icon={faFacebook} />
-                        </a>
-                        <a id='ic'  href="https://instagram.com" target='_blank'>
-                            <FontAwesomeIcon icon={faInstagram} />
-                        </a>
-                        <a id='ic'  href="https://twitter.com" target='_blank'>
-                            <FontAwesomeIcon icon={faTwitter} />
-                        </a>
-                    </div>
-
-                    <div className='col1'>
-                        <h3 className='colad'>15, West End Lane UB3 5LT</h3>
-                    </div>
-
-
-                    <div className='col'>
-                        <h4 className='cont'>07828378978</h4>
-                    </div>
-
-
-
-                    <div className='col'>
-                        <h4 className='cont'>raunak@liv-ex.com</h4>
-                    </div>
-
-                    <div className='logo'>
-                    <img className="logo3" src={logo} alt="" />
-
-                    </div>
-
-
-                </div>
-
-                <hr />
-
-
-                <div className='row2'>
-                    <p className='col-sm'>
-                        &copy;{new Date().getFullYear()}. All Rights Reserved.
-                    </p>
-
-                </div>
-
-
-
-
-            </div>
-
+  return (
+    <div className="main-footer">
+      <div className="footercontainer">
+        <div className="Socials">
+          <h3 className="socialsH3">Socials Follow: </h3>
+          <a id="ic" href="https://facebook.com" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a id="ic" href="https://instagram.com" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a id="ic" href="https://twitter.com" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
         </div>
-    )
-}
 
-export default Footer
+        <div className="row2">
+          <h3 className="col-sm">
+            &copy;{new Date().getFullYear()}. All Rights Reserved.
+          </h3>
+        </div>
+
+        <div className="col1">
+          <h3 className="colh3">Contact Us:</h3>
+          <h4 className="cont">15, West End Lane UB3 5LT</h4>
+          <h4 className="cont">07828378978</h4>
+          <h4 className="cont">raunak@liv-ex.com</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- loginsignup/src/Routes/Home.js
+++ loginsignup/src/Routes/Home.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { ReactDOM } from "react-dom/client";
 import "../Styles/Home.css";
+import { Link } from "react-router-dom";
 
 import logo from "../Assets/ITHome.jpg";
+import image from "../Assets/Liv-ex.jpg";
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="container-home">
-        <img className="home-image" src={logo} alt="" />
-        <div className="overlay">
-          <p className="home-text">Welcome to IT SUPPORT</p>
+      <div
+        className="top-container"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <h1 className="heading-home">
+          Your Ultimate Destination <br />
+          for Expert IT Support Solutions
+        </h1>
+        <br />
+        <p className="p-home">
+          Elevating IT Support. Your one-stop solution <br />
+          for expert tech assistance, ensuring seamless <br />
+          and secure experiences in the digital realm.
+        </p>
+      </div>
+
+      <div className="middle-container">
+        <h2 className="h2-home">
+          Join the Tech Revolution:{" "}
+          <strong className="st-home">Sign Up Now!</strong>
+        </h2>
+
+        <div className="nav-button">
+          <Link to="/SignUp">
+            <button className="navbarbutton">Sign Up</button>
+          </Link>
         </div>
       </div>
     </div>
